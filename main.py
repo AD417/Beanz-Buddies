75d665bb16ded552d247f1d993bc4a7caaee30f2
Alexander Day <55056986+AD417@users.noreply.github.com>
2024-07-12 21:43:05 -0400

    Finish API for getting user data from slack. It's really slow.

--- main.py
+++ main.py
@@ -1,58 +1,50 @@
 from datetime import date
-from CRUD import *
+from slack.read import *
 
 from datetime import datetime
 
 start = datetime.now()
 
-db = Database()
+def main():
+    members = set(members_of_channel(CHANNEL_ID))
+    frosh = set(members_of_group(FROSH_ID))
+    active = set(members_of_group(ACTIVE_ID))
+    if not members:
+        print("Error getting members info.")
+        return
+    
+    alumni = members.difference(frosh, active)
+    frosh.intersection_update(members)
+    active.intersection_update(members)
+    
+    print("Freshmen:")
+    for member in frosh:
+        user_info = get_user_info(member)
+        if not user_info:
+            print(f"Failed to get info for member {member}.")
+            continue
 
-names = [
-    "Benjamin Piro",
-    "Katie Koontz",
-    "Nidhi Baindur",
-    "Joe Abbate",
-    "Dani Saba",
-    "Joe Vita",
-    "Chrissy Espeleta",
-    "Connor Langa",
-    "Zachary Cox",
-    "Ian Kopke",
-    "Sam Cordry",
-    "Grant Hawerlander",
-    "Eva",
-    "Ekam",
-    "Ethan Ferguson",
-    "Mary Strodl",
-    "Vivian Hafener",
-    "Mirai Day",
-    "Asha Kadagala",
-    "Emma Schmidt",
-    "Gavin McConnell",
-    "Jason Koser",
-    "Jinna Smail",
-    "Joseph Issac",
-    "Matt Marafino",
-    "Will Hellinger",
-    "Wilson McDade",
-    "Adam Newlight",
-    "Darwin Tran",
-    "Tyler Samay",
-    "Ella Soccoli",
-    "Charlotte George",
-]
+        print(user_info["real_name"])
 
-print(names)
+    print("\nUpperclassmen:")
+    for member in active:
+        user_info = get_user_info(member)
+        if not user_info:
+            print(f"Failed to get info for member {member}.")
+            continue
 
-db.add_pair("Liriel Bryer", "Emily Caston")
+        print(user_info["real_name"])
 
-pairs = generate_pairs(db, names, date.today())
+    print("\nAlumni:")
+    for member in alumni:
+        user_info = get_user_info(member)
+        if not user_info:
+            print(f"Failed to get info for member {member}.")
+            continue
 
-for pair in pairs: 
-    print(pair)
-    db.add_pair(*pair)
+        print(user_info["real_name"])
 
-db.close()
+main()
 
 end = datetime.now()
 
---
+++ slack/read.py
@@ -0,0 +1,71 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SLACK_TOKEN = os.getenv("SLACK_API_KEY")
+CHANNEL_ID = os.getenv("BEANZ_CHANNEL_ID") # the ID of #beanz-buddies
+ACTIVE_ID = os.getenv("ACTIVE_ID") # Active User ID
+FROSH_ID = os.getenv("FROSH_ID") # Freshman User ID
+
+def members_of_channel(channel_id):
+    """Get a list of people in the specified channel."""
+    url = 'https://slack.com/api/conversations.members'
+    params = {
+        'channel': channel_id,
+        'limit': 1000  # Adjust limit as necessary
+    }
+    headers = {
+        'Authorization': f'Bearer {SLACK_TOKEN}'
+    }
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status code {response.status_code}")
+    data = response.json()
+    print(data)
+    if not data['ok']:
+        raise Exception(f"Error getting channel members: {data['error']}")
+    
+    return data['members']
+
+def members_of_group(user_group_id):
+    """Get a list of people in the specified user group (Frosh, Active)"""
+    url = 'https://slack.com/api/usergroups.users.list'
+    params = {
+        'usergroup': user_group_id
+    }
+    headers = {
+        'Authorization': f'Bearer {SLACK_TOKEN}'
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data['ok']:
+            return data['users']
+        else:
+            raise Exception(f"Error getting user group members: {data['error']}")
+    else:
+        raise Exception(f"Request failed with status code {response.status_code}")
+
+def get_user_info(user_id):
+    """Get specific user info regarding a specific person."""
+    url = 'https://slack.com/api/users.info'
+    params = {
+        'user': user_id
+    }
+    headers = {
+        'Authorization': f'Bearer {SLACK_TOKEN}'
+    }
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        print(f"Request failed with status code {response.status_code}")
+        return None
+    
+    data = response.json()
+    if not data['ok']:
+        raise Exception(f"Error getting user info: {data['error']}")
+    
+    return data['user']
